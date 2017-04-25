6ed3733cf59fe56c670eb44d63ea58b24d8c071e
Matthew Brown <mattd429@gmail.com>
2017-04-25 18:13:18 -0400

    Update JDBCMySQLConnection.java
    added to "." "."

--- src/JDBCMySQLConnection.java
+++ src/JDBCMySQLConnection.java
@@ -1,7 +1,7 @@
 package com.theopentutorials.jdbc.db;
 
 //step 1: use interfaces from java.sql package
-import java sql Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
